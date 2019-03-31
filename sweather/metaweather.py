cfe7bf5b9252b2a3888960489473cf99536f47a5
Łukasz Drabek <lpdrabek@gmail.com>
2019-03-31 21:41:13 +0200

    more features

--- sweather/metaweather.py
+++ sweather/metaweather.py
@@ -1,2 +1,15 @@
 # https://www.metaweather.com/api/
+import requests
+
+
+API_URL = 'https://www.metaweather.com/api'
+
+
+def search(city):
+    req = requests.get("{api_url}/location/search/?query={city}".format(
+                api_url=API_URL, city=city))
+    if req.status_code != 200:
+        return (0, 'API returned'+req.status_code)
+    else:
+        return req.json()
 
--- sweather/views.py
+++ sweather/views.py
@@ -1,13 +1,32 @@
-from flask import render_template
+from flask import render_template, request
+import json
+
+from pprint import pprint
 
 from sweather import app
+from sweather.metaweather import search
 
 
 @app.route('/')
 def index():
-    pass
+    return render_template('index.html')
 
-    
-@app.route('/w')
+
+@app.route('/city_select')
 def weather():
-    return render_template('weather.html')
+    cities = request.args.get('cities')
+    if not cities:
+        return render_template('skel.html')
+    c_list = cities.split('\r\n')
+    all_cities = {}
+    for city in c_list:
+        a = search(city)
+        print(a[0]['title'])
+        all_cities[city] = search(city)
+    pprint(all_cities)
+    return render_template('weather.html', cities=all_cities)
+
+
+@app.route('/weather')
+def index():
+    return render_template('index.html')
